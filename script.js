b8dd6f7d3be6779bde96226fab2b5288abfe944f
Aydin06-2010 <aydinabdullahaziz@gmail.com>
2024-12-13 18:19:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -138,7 +138,6 @@ const Element = document.getElementById(ElementID);
 const DivElement = document.getElementById(DivID); 
 DivElement.style.display = "flex";
 Element.style.color = "#72a9d5"; 
-consle.log(`${ElementID} ${DivID}`);
 }
 
 function HideAllDiv(){
