6df56b0b6467c31f28401806bc14c8b07c9b6415
Aydin06-2010 <aydinabdullahaziz@gmail.com>
2024-12-15 20:30:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -402,7 +402,7 @@ window.addEventListener("resize", handleResize);
 
 
 // Set the event date in UTC format
-const eventDateUTC = new Date("2024-12-16T07:00:00+06:00").getTime();
+const eventDateUTC = new Date(Date.UTC(2024, 11, 16, 7, 0, 0)); // new Date("2024-12-16T07:00:00+06:00").getTime();
 // Note: Month is 0-indexed, so 11 = December
 
 function updateCountdown() {
