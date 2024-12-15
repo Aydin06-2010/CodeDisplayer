d6465e6f788174714e417da4413757a29fc34664
Aydin06-2010 <aydinabdullahaziz@gmail.com>
2024-12-15 18:36:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -402,7 +402,7 @@ window.addEventListener("resize", handleResize);
 
 
 // Set the event date in UTC format
-const eventDateUTC = new Date("2024-12-15T18:20:00+06:00").getTime();
+const eventDateUTC = new Date("2024-12-15T18:40:00+06:00").getTime();
 // Note: Month is 0-indexed, so 11 = December
 
 function updateCountdown() {
@@ -410,11 +410,13 @@ function updateCountdown() {
 const nowUTC = new Date().getTime();
 const distance = eventDateUTC - nowUTC;
 
-if (distance < 0) {
-document.getElementById("countdown").textContent = "The event has started!";
-document.getElementById("TimerDiv").style.display = "none"; 
+if (distance <= 0) {
+HideTimerDiv(); 
 clearInterval(interval);
 return;
+}
+else if(distance > 0){
+ShowTimerDiv(); 
 }
 
 const days = Math.floor(distance / (1000 * 60 * 60 * 24));
